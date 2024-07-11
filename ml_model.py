be42f09c847e666d300cc06a587b11b82142529d
AngadSinghLamba <angadsinghlamba@gmail.com>
2024-07-11 22:50:23 +0530

    added 3 new ml model code

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,14 @@
 print("adding all ML related code here")
+
+def model1():
+    print("addimg code for ML Model1")
+
+
+def model2():
+    print("addimg code for ML Model2")
+
+
+
+
+def model3():
+    print("addimg code for ML Model3")
